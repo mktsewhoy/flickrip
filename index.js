d60b560e8e25a5536ba9892a8f68f56df73c5d6f
Matthew Sew Hoy <mktsewhoy@hotmail.co.nz>
2019-06-27 14:45:11 +1200

    Remove text field from form.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 function showOutput() {
     var urlOutput = document.getElementById("url").value;
     if (urlOutput == "") {
-    // if (urlOutput == "" || checkFlickr(urlOutput) == false) {
+    // if (urlOutput == "" || checkURL(urlOutput) == false) {
         urlOutput = "Please enter a valid link."
     }
     displayLink.innerHTML = urlOutput;
 }
 
+function detectEnter(e) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) { //Enter keycode
+        // alert("Sending your Message : " + document.getElementById('txt').value);
+        showOutput()
+    }
+}
+
 // function checkFlickr(url) {
-//     var check = true;
+//     var check = false;
 //     if (url == "" || ) {
 //         urlOutput = "Please enter a valid link."
 //     }
-//     // else {}
+//     // else {check = true}
 // }
